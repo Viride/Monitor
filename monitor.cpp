d25be64c904fe0978ca2610cc21104054cdb5518
Paweł Kaczmarek <pawel.kaczmarek@ingonyama.pl>
2018-05-28 11:39:30 +0200

    next day, solve state

--- monitor.cpp
+++ monitor.cpp
@@ -34,11 +34,6 @@ class Monitor
     //void put  //wkłada element
     void put()
     {
-        printf("Przed wysłaniem\t");
-        s_sendmore(publisher, "B");
-        s_send(publisher, "We would like to see this");
-        printf("Po wysłaniu\n");
-        sleep(1);
     }
     //void pop      //zdejmuje element
     void pop()
@@ -47,35 +42,53 @@ class Monitor
 
     void listen()
     {
-        int i=0;
+        zmq::context_t context(1);
+        zmq::socket_t subscriber(context, ZMQ_SUB);
+        subscriber.connect("tcp://localhost:5555");
+        subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+        zmq::message_t message;
         printf("Zaczynam słuchać\n");
-        while(i<5)
+        while (state != END)
         {
+            printf("State: %d\n", state);
             //Read envelope with address
+            //subscriber.recv(&message);
             printf("Odbieram\t");
             std::string address = s_recv(subscriber);
             printf("Wciąż odbieram \t");
             std::string contents = s_recv(subscriber);
             printf("Odebrałem\n");
             std::cout << "[" << address << "] " << contents << std::endl;
-            i++;
         }
         printf("słuchałem\n");
     }
+    void send()
+    {
+        zmq::context_t context(1);
+        zmq::socket_t publisher(context, ZMQ_PUB);
+        publisher.bind("tcp://*:5555");
 
-    zmq::context_t context;
-    zmq::socket_t subscriber, publisher;
+        while (state != END)
+        {
+            printf("State: %d\n", state);
+            printf("Przed wysłaniem\t");
+            s_sendmore(publisher, "B");
+            s_send(publisher, "We would like to see this");
+            printf("Po wysłaniu\n");
+            sleep(1);
+        }
+    }
 
-    Monitor() : context(1), subscriber(context, ZMQ_SUB), publisher(context, ZMQ_PUB)
+    //zmq::socket_t publisher;
+    //zmq::socket_t subscriber, publisher;
+
+    Monitor()
     {
         pool = new int[10];
+        state = IDLE;
         //context = zmq::context_t(1);
         //subscriber = zmq::socket_t(context, ZMQ_SUB);
         //publisher = zmq::socket_t(context, ZMQ_PUB);
-        subscriber.connect("tcp://localhost:5555");
-        subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
-
-        publisher.bind("tcp://*:5555");
 
         printf("Initialized\n");
         pid_t pid = fork();
@@ -84,20 +97,44 @@ class Monitor
         {
             listen();
         }
+        else
+        {
+            pid_t pid2 = fork();
+            if (pid2 == 0)
+            {
+                send();
+            }
+        }
         //Monitor::publisher = zmq::socket(context, zmqpp::socket_type::publish);
         // subscriber(context, zmqpp::socket_type::subscribe);
         // publisher(context, zmqpp::socket_type::publish);
     }
+    ~Monitor()
+    {
+        printf("Odpaliłem się");
+        state = END;
+        ///CZEKANIE AŻ PROCESY SIĘ SKOŃCZĄ
+        // int status;
+        // do
+        // {
+        //     status = wait();
+        //     if (status == -1 && errno != ECHILD)
+        //     {
+        //         perror("Error during wait()");
+        //         abort();
+        //     }
+        // } while (status > 0);
+    }
+
+    volatile int state;
 
   private:
-    
     //static zmq::context_t context;
     //zmq::message message_recv, message_send;
     int *pool;
     int i_get;
     int i_put;
     int count;
-    int state;
 
     vector<int> Rn;
 };
@@ -105,70 +142,9 @@ class Monitor
 int main()
 {
     Monitor monitor;
-    // zmqpp::context context;
-    // zmqpp::socket subscriber(context, zmqpp::socket_type::subscribe);
-    // zmqpp::socket publisher(context, zmqpp::socket_type::publish);
-    // printf("Jestem w mainie\n");
-    // sleep(3);
-    // for(int i=0 i<10;i++){
-    //     snprintf ((char *) message.data(), 20 ,
-    //         "%d %d %d", 10, 10, 10);
-    //     publisher.send(message);
-    // }
     sleep(2);
+    monitor.state = END;
+    // free(monitor);
     printf("jestem\n");
-    // monitor.put();
-    // monitor.put();
-    // monitor.put();
-    // monitor.put();
-    // monitor.put();
-    // pid_t pid = fork();
-    // printf("%d\n", pid);
-    // if (pid == 0)
-    // {
-    //     printf("jestem1\n");
-
-    //     zmq::context_t context(1);
-    //     zmq::socket_t publisher(context, ZMQ_PUB);
-    //     publisher.bind("tcp://*:5563");
-    //     int i = 0;
-    //     sleep(1);
-    //     while (i < 5)
-    //     {
-    //         //  Write two messages, each with an envelope and content
-    //         s_sendmore(publisher, "A");
-    //         s_send(publisher, "We don't want to see this");
-    //         s_sendmore(publisher, "B");
-    //         s_send(publisher, "We would like to see this");
-    //         sleep(1);
-    //         i++;
-    //     }
-    //     printf("koniec1");
-        
-    // }
-    // else
-    // {
-    //     printf("jestem2\n");
-
-    //     //  Prepare our context and publisher
-    //     zmq::context_t context(1);
-    //     zmq::socket_t subscriber(context, ZMQ_SUB);
-    //     subscriber.connect("tcp://localhost:5563");
-    //     subscriber.setsockopt(ZMQ_SUBSCRIBE, "B", 1);
-    //     int i=0;
-    //     while (i<5)
-    //     {
-
-    //         //  Read envelope with address
-    //         std::string address = s_recv(subscriber);
-    //         //  Read message contents
-    //         std::string contents = s_recv(subscriber);
-
-    //         std::cout << "[" << address << "] " << contents << std::endl;
-    //         i++;
-    //     }
-    //     printf("koniec2");
-    //}
-
     return 0;
 }
